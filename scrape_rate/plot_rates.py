41e4021a714e57b92e9aa9357ffe0fd5457fbc0f
mlvdl <mrtlpzvdl@proton.me>
2025-01-08 22:37:13 +0100

    interactive image

--- scrape_rate/plot_rates.py
+++ scrape_rate/plot_rates.py
@@ -2,47 +2,56 @@ from typing import Tuple
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
+
 from loguru import logger
 
 from scrape_rate.config import DATA_DIR
 
 
 def plot_rates() -> None:
-    styles = ['ggplot', 'dark_background']
+    styles = ['plotly', 'plotly_dark']
     for style in styles:
         plot_in_style(style)
 
 
 def plot_in_style(style: str) -> None:
     df, labels_df = get_dataframes()
-    plt.style.use(style=style) 
     
-    fig = plt.figure(figsize=(10, 7))
-    ax = plt.subplot(111)
+    fig = go.Figure()
     for column in df.columns:
         if column in labels_df['fundName'].tolist():
-            ax.plot(df.index, df[column], label=column.split()[0])
-            if len(df.index) > 1:
-                ax.text(x=df.index[-1] + (df.index[-1] - df.index[0]) / 100, y=df[column].iloc[-1], s=str(df[column].iloc[-1]))
-            else:
-                ax.text(x=df.index[-1], y=df[column].iloc[-1], s=str(df[column].iloc[-1]))
+            fig.add_trace(go.Scatter(x=df.index, y=df[column],
+                                     mode='lines+markers',
+                                     name=column.split()[0],
+                                     text=df[column],
+                                     hoverinfo='y'))
 
-    ax = plt.gca()
-    ax.xaxis.set_major_locator(mdates.DayLocator())  # Set major ticks to one per day
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+            fig.add_annotation(x=df.index[-1], y=df[column].iloc[-1], text=df[column].iloc[-1])
 
-    plt.title('Interest rate over time', fontsize=16)
-    plt.ylabel('Rate', fontsize=14)
-    plt.legend(title='Rates', fontsize=12)
+    fig.update_layout(
+        title='Interest rate over time',
+        xaxis_title='Date',
+        yaxis_title='Rate',
+        xaxis=dict(
+            tickformat='%Y-%m-%d',
+            showgrid=True,
+            zeroline=False,
+            rotate=45,
+        ),
+        legend_title='Rates',
+        template=style
+    )
 
-    plt.xlim(df.index[0] - (df.index[-1] - df.index[0]) / 100, df.index[-1] + (df.index[-1] - df.index[0]) / 10)
-    plt.xticks(rotation=45)
 
-    ax.legend(loc='best')
+    fig.update_traces(marker=dict(size=1), hoverlabel=dict(bgcolor="white", font_size=13, font_family="Rockwell"))
 
-    fig_path = DATA_DIR / f"rates_{style}.png"
-    plt.savefig(fig_path)
-    plt.close()
+    fig_path = DATA_DIR / f"rates_{style}.html"
+    fig.write_html(fig_path)
+    fig.write_image(fig_path.with_suffix('.png'))
+    # fig.show()
 
     logger.info(f"Rates plotted to {fig_path}") 
 
