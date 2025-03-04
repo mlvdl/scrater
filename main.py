054983fc593d6be20c2734acc04bc7b8ef4c6a90
mlvdl <mrtlpzvdl@proton.me>
2025-03-04 14:28:41 +0100

    add plots

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from scrape_rate.plot_interactive_average import plot_average_rates
 from scrape_rate.plot_interactive_rates import plot_rates
 from scrape_rate.update_rates import update_rates
 
 
-if __name__ == '__main__':
+def main(average: bool = False) -> None:
     update_rates()
     plot_rates()
+    plot_average_rates()
+
+
+if __name__ == '__main__':
+    main()
+
